11f4a11e9549c8a6d5f24bfc509db448b0508ab5
Daniel Holthus <danielholthus@Daniels-MacBook-Pro.local>
2024-02-21 22:42:21 -0600

    project size and code cleanup

--- js/app.js
+++ js/app.js
@@ -1,126 +1,71 @@
-// //if page reloads, always scroll back to splash page
-// if (history.scrollRestoration) {
-//     history.scrollRestoration = 'manual';
-// } else {
-//     window.onbeforeunload = function () {
-//         window.scrollTo(0, 0);
-//     }
-// }
-
-//checking if user screen is a touch screen
-
-let hasTouchScreen = false;
+let hasTouchScreen = false
 if ("maxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.maxTouchPoints > 0;
+  hasTouchScreen = navigator.maxTouchPoints > 0
 } else if ("msMaxTouchPoints" in navigator) {
-    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+  hasTouchScreen = navigator.msMaxTouchPoints > 0
 } else {
-    let mQ = window.matchMedia && matchMedia("(pointer:coarse)");
-    if (mQ && mQ.media === "(pointer:coarse)") {
-        hasTouchScreen = !!mQ.matches;
-    } else if ('orientation' in window) {
-        hasTouchScreen = true; // deprecated, but good fallback
-    } else {
-        // Only as a last resort, fall back to user agent sniffing
-        let UA = navigator.userAgent;
-        hasTouchScreen = (
-            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
-            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
-        );
-    }
+  let mQ = window.matchMedia && matchMedia("(pointer:coarse)")
+  if (mQ && mQ.media === "(pointer:coarse)") {
+    hasTouchScreen = !!mQ.matches
+  } else if ("orientation" in window) {
+    hasTouchScreen = true // deprecated, but good fallback
+  } else {
+    // Only as a last resort, fall back to user agent sniffing
+    let UA = navigator.userAgent
+    hasTouchScreen =
+      /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+      /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+  }
 }
 
-let isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
-let pageWidth = window.innerWidth;
-
-// -----
-// enable scroll effect from navbar items without adding anything to URL
-
-//Get all the hyperlink elements
-let links = document.getElementsByTagName("a");
-//Browse the previously created array
-Array.prototype.forEach.call(links, function(elem, index) {
-  //Get the hyperlink target and if it refers to an id go inside condition
-  let elemAttr = elem.getAttribute("href");
-  if(elemAttr && elemAttr.includes("#")) {
-    //Replace the regular action with a scrolling to target on click
-    elem.addEventListener("click", function(ev) {
-      ev.preventDefault();
-      //Scroll to the target element using replace() and regex to find the href's target id
-      document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-          inline: "nearest"
-          });
-    });
-  }
-});
+let isSafari =
+  /constructor/i.test(window.HTMLElement) ||
+  (function (p) {
+    return p.toString() === "[object SafariRemoteNotification]"
+  })(
+    !window["safari"] ||
+      (typeof safari !== "undefined" && window["safari"].pushNotification)
+  )
+let pageWidth = window.innerWidth
 
 // -----
 // event listeners
 
-if (!(hasTouchScreen)) {
-    window.addEventListener("resize", function() {
-        window.location.reload();
-    });
+if (!hasTouchScreen) {
+  window.addEventListener("resize", function () {
+    window.location.reload()
+  })
 }
 
-let nav = document.getElementById("nav");
+let nav = document.getElementById("nav")
 if (hasTouchScreen) {
-    nav.style.display = "none";
+  nav.style.display = "none"
 }
 
-let logo = document.querySelector('.name-logo');
+let logo = document.querySelector(".name-logo")
 if (isSafari) {
-    logo.style.transform = "translate(-50%, -50%)";
+  logo.style.transform = "translate(-50%, -50%)"
 
-    if (pageWidth <= 330) {
-        logo.style.fontSize = "16px";
-    }
-    else if (pageWidth <= 420) {
-        logo.style.fontSize = "38px";
-    }
-    else if (pageWidth <= 480) {
-        logo.style.fontSize = "46px";
-    }
-    else if (pageWidth <= 550) {
-        logo.style.fontSize = "56px";
-    }
-    else if (pageWidth <= 650) {
-        logo.style.fontSize = "66px";
-    }
-    else if (pageWidth <= 760) {
-        logo.style.fontSize = "76px";
-    }
-    else if (pageWidth <= 900) {
-        logo.style.fontSize = "90px";
-    }
-    else if (pageWidth <= 1080) {
-        logo.style.fontSize = "112px";
-    }
-    else {
-        logo.style.fontSize = "136px";
-    }
+  if (pageWidth <= 330) {
+    logo.style.fontSize = "16px"
+  } else if (pageWidth <= 420) {
+    logo.style.fontSize = "38px"
+  } else if (pageWidth <= 480) {
+    logo.style.fontSize = "46px"
+  } else if (pageWidth <= 550) {
+    logo.style.fontSize = "56px"
+  } else if (pageWidth <= 650) {
+    logo.style.fontSize = "66px"
+  } else if (pageWidth <= 760) {
+    logo.style.fontSize = "76px"
+  } else if (pageWidth <= 900) {
+    logo.style.fontSize = "90px"
+  } else if (pageWidth <= 1080) {
+    logo.style.fontSize = "112px"
+  } else {
+    logo.style.fontSize = "136px"
+  }
 }
 if (hasTouchScreen || window.innerWidth < 760 || isSafari) {
-    logo.style.position = "absolute";
+  logo.style.position = "absolute"
 }
-logo.addEventListener("click", function() {
-    window.scrollTo(0, 0)
-});
-
-let mobileLogo = document.querySelector('.name-logo-alt');
-mobileLogo.addEventListener("click", function() {
-    window.scrollTo(0, 0)
-});
-
-let scrollarrow = document.querySelector('.scrollarrow');
-scrollarrow.addEventListener("click", function() {
-    document.getElementById('projects').scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-        inline: "nearest"
-    });
-});
-
-// ----
--- js/homeAnimations.js
+++ js/homeAnimations.js
@@ -1,225 +1,225 @@
 function nameLogoAnimation() {
-    //timeline for name logo
-
-    let tl1 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home',
-            start: '0%',
-            end: '35%',
-            scrub: 1
-        }
-    });
-    if (!(hasTouchScreen) && pageWidth > 760 && !(isSafari)){
-        if (pageWidth < 1000) {
-            tl1.to('.name-logo', {scale: 1, cursor: 'pointer', x: '-36vw', y: '-46vh'});
-        } 
-        else if (pageWidth < 1200) {
-            tl1.to('.name-logo', {scale: 1, cursor: 'pointer', x: '-38vw', y: '-46vh'});
-        }
-        else if (pageWidth < 1400) {
-            tl1.to('.name-logo', {scale: 1, cursor: 'pointer', x: '-39vw', y: '-46vh'});
-        } 
-        else if (pageWidth < 1500) {
-            tl1.to('.name-logo', {scale: 1, cursor: 'pointer', x: '-40vw', y: '-46vh'});
-        }
-        else {
-            tl1.to('.name-logo', {scale: 1, cursor: 'pointer', x: '-42vw', y: '-46vh'});
-        }
-    }
-    else {
-        tl1.to('.name-logo', {opacity: 0, display: 'hidden'}).to('.name-logo-alt', {opacity: 1, cursor: 'pointer'});
-    }
+  //timeline for name logo
+  let tl1 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "0%",
+      end: "50%",
+      scrub: 1.2,
+    },
+  })
+  if (!hasTouchScreen && pageWidth > 760 && !isSafari) {
+    tl1.to(".name-logo", {
+      cursor: "pointer",
+      left: "5%",
+      top: "2%",
+      transform: "scale(1)",
+    })
+  } else {
+    tl1
+      .to(".name-logo", { opacity: 0, display: "hidden" })
+      .to(".name-logo-alt", { opacity: 1, cursor: "pointer" })
+  }
 }
 
-nameLogoAnimation();
+nameLogoAnimation()
 
 // load all animations except for name animation AFTER the dom loads
 
 window.onload = function () {
-    
-    //timeline for home square
-    
-    let tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home',
-            start: '0%',
-            end: '60%',
-            scrub: 0.3
-        }
-    });
-    
-    tl2.to('.home-square', {rotation: 45, x: '100%', opacity: 0});
-    
-    //timeline for typing text
-    
-    let tl3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home',
-            start: '0%',
-            end: '50%',
-            scrub: 0.3
-        }
-    });
-    
-    tl3.to('.typing-text-container', {opacity: 0});
-    
-    //timeline for name background
-    
-    let tl4 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.home',
-            start: '0%',
-            end: '50%',
-            scrub: 0.3
-        }
-    });
-    
-    tl4.to('.name-background', {opacity: 0});
-    
-    //timeline for project 1
-    
-    let tl5 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.project-1',
-            start: '30% bottom',
-            end: '60% bottom',
-            scrub: 1,
-        }
-    });
-    
-    tl5.from('.project-1', {opacity: 0, x: '-40px'});
-    
-    //timeline for project 2
-    
-    let tl5b = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.project-2',
-            start: '40% bottom',
-            end: '70% bottom',
-            scrub: 1,
-        }
-    });
-    
-    tl5b.from('.project-2', {opacity: 0, x: '40px'});
-    
-    //timeline for project 3 
-    
-    let tl6 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.project-3',
-            start: '30% bottom',
-            end: '60% bottom',
-            scrub: 1,
-        }
-    });
-    
-    tl6.from('.project-3', {opacity: 0, x: '-40px'});
-    
-    //timeline for project 4
-    
-    let tl6b = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.project-4',
-            start: '40% bottom',
-            end: '70% bottom',
-            scrub: 1,
-        }
-    });
-    
-    tl6b.from('.project-4', {opacity: 0, x: '40px'});
+  //timeline for home square
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "0%",
+      end: "60%",
+      scrub: 0.1,
+    },
+  })
 
-    //timeline for project 5
+  tl2.to(".home-square", { rotation: 45, x: "100%", opacity: 0 })
 
-    let tl7 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.project-5',
-            start: '30% bottom',
-            end: '60% bottom',
-            scrub: 1,
-        }
-    });
-    
-    tl7.from('.project-5', {opacity: 0, x: '-40px'});
+  //timeline for typing text
+  let tl3 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "0%",
+      end: "50%",
+      scrub: 0,
+    },
+  })
 
-        //timeline for project 6
-    
-        let tl7b = gsap.timeline({
-            scrollTrigger: {
-                trigger: '.project-6',
-                start: '40% bottom',
-                end: '70% bottom',
-                scrub: 1,
-            }
-        });
-        
-        tl7b.from('.project-6', {opacity: 0, x: '40px'});
-    
-    //timeline for projects square 
-    
-    let tl8 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.projects',
-            start: 'top bottom',
-        },
-        repeat: -1
-    });
-    
-    tl8.to('.projects-square', {duration: 70, x: '102vw', y: '25vh', rotation: 340, ease:'linear'})
-        .to('.projects-square', {duration: 70, x: '-60vw', y: '25vh', rotation: -360, ease: 'linear'});
-    
-    // timeline for projects triangle
-    
-    let tl9 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.projects-triangle',
-            start: 'top bottom',
-        },
-        repeat: -1,
-    });
-        
-    tl9.to('.projects-triangle', {duration: 70, x: '-102vw', y: '-20vh', rotation: -340, ease:'linear'})
-        .to('.projects-triangle', {duration: 80, x: '100vw', y: '20vh', rotation: 360, ease: 'linear'});
-    
-    // timeline for projects cross
-    
-    let tl10 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.projects-cross',
-            start: 'top bottom',
-        },
-        repeat: -1
-    });
-    
-    
-    tl10.to('.projects-cross', {duration: 80, x: '102vw', y: '-40vh', rotation: 340, ease:'linear'})
-        .to('.projects-cross', {duration: 90, x: '-100vw', y: '20vh', rotation: -360, ease: 'linear'});
-    
-    //timeline for headshot
-    
-    let tl11 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.about',
-            start: 'top 90%',
-            end: 'bottom top',
-            scrub: true,
-        },
-    });
-    
-    tl11.from('.headshot', {x: '70px', y: '-30px'});
-    
-    // timeline for about me content
-    
-    let tl12 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.about',
-            start: 'top 70%',
-            end: 'bottom bottom',
-            scrub: true,
-        },
-    });
-    
-    tl12.from('.about-text', {x: '-40px', y: '40px'});
-    
-    // timeline for contact animation
+  tl3.to(".typing-text-container", { opacity: 0 })
 
+  //timeline for name background
+  let tl4 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".home",
+      start: "0%",
+      end: "50%",
+      scrub: 0,
+    },
+  })
+
+  tl4.to(".name-background", { opacity: 0 })
+
+  //timeline for project 1
+  let tl5 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-1",
+      start: "30% bottom",
+      end: "60% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl5.from(".project-1", { opacity: 0, x: "-40px" })
+
+  //timeline for project 2
+  let tl5b = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-2",
+      start: "40% bottom",
+      end: "70% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl5b.from(".project-2", { opacity: 0, x: "40px" })
+
+  //timeline for project 3
+  let tl6 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-3",
+      start: "30% bottom",
+      end: "60% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl6.from(".project-3", { opacity: 0, x: "-40px" })
+
+  //timeline for project 4
+  let tl6b = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-4",
+      start: "40% bottom",
+      end: "70% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl6b.from(".project-4", { opacity: 0, x: "40px" })
+
+  //timeline for project 5
+  let tl7 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-5",
+      start: "30% bottom",
+      end: "60% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl7.from(".project-5", { opacity: 0, x: "-40px" })
+
+  //timeline for project 6
+  let tl7b = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".project-6",
+      start: "40% bottom",
+      end: "70% bottom",
+      scrub: 0.5,
+    },
+  })
+  tl7b.from(".project-6", { opacity: 0, x: "40px" })
+
+  //timeline for projects square
+  let tl8 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".projects",
+      start: "top bottom",
+    },
+    repeat: -1,
+  })
+  tl8
+    .to(".projects-square", {
+      duration: 70,
+      x: "102vw",
+      y: "25vh",
+      rotation: 340,
+      ease: "linear",
+    })
+    .to(".projects-square", {
+      duration: 70,
+      x: "-60vw",
+      y: "25vh",
+      rotation: -360,
+      ease: "linear",
+    })
+
+  // timeline for projects triangle
+  let tl9 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".projects-triangle",
+      start: "top bottom",
+    },
+    repeat: -1,
+  })
+  tl9
+    .to(".projects-triangle", {
+      duration: 70,
+      x: "-102vw",
+      y: "-20vh",
+      rotation: -340,
+      ease: "linear",
+    })
+    .to(".projects-triangle", {
+      duration: 80,
+      x: "100vw",
+      y: "20vh",
+      rotation: 360,
+      ease: "linear",
+    })
+
+  // timeline for projects cross
+  let tl10 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".projects-cross",
+      start: "top bottom",
+    },
+    repeat: -1,
+  })
+  tl10
+    .to(".projects-cross", {
+      duration: 80,
+      x: "102vw",
+      y: "-40vh",
+      rotation: 340,
+      ease: "linear",
+    })
+    .to(".projects-cross", {
+      duration: 90,
+      x: "-100vw",
+      y: "20vh",
+      rotation: -360,
+      ease: "linear",
+    })
+
+  //timeline for headshot
+  let tl11 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top 90%",
+      end: "bottom top",
+      scrub: true,
+    },
+  })
+  tl11.from(".headshot", { x: "70px", y: "-30px" })
+
+  // timeline for about me content
+  let tl12 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".about",
+      start: "top 70%",
+      end: "bottom bottom",
+      scrub: true,
+    },
+  })
+  tl12.from(".about-text", { x: "-40px", y: "40px" })
 }
